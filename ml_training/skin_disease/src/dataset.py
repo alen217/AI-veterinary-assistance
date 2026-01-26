58a95800f5dd262fa0542fdf7094d66959427b63
alen217 <alen217fist@gmail.com.com>
2026-01-26 12:56:26 +0530

    Improve skin disease model with transfer learning and fine-tuning 78%

--- ml_training/skin_disease/src/dataset.py
+++ ml_training/skin_disease/src/dataset.py
@@ -1,34 +1,48 @@
 import os
-from torchvision import datasets, transforms
+from torchvision import datasets
 from torch.utils.data import DataLoader
 
+def load_data(
+    data_dir,
+    batch_size,
+    train_transform=None,
+    val_transform=None
+):
+    train_path = os.path.join(data_dir, "train")
+    val_path = os.path.join(data_dir, "val")
+    test_path = os.path.join(data_dir, "test")
 
-def get_transforms(train=True):
-    if train:
-        return transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(15),
-            transforms.ColorJitter(brightness=0.2, contrast=0.2),
-            transforms.ToTensor(),
-        ])
-    return transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-    ])
-
-
-def load_data(data_dir, batch_size=16):
-    train = datasets.ImageFolder(os.path.join(data_dir, "train"),
-                                 transform=get_transforms(True))
-    val = datasets.ImageFolder(os.path.join(data_dir, "val"),
-                               transform=get_transforms(False))
-    test = datasets.ImageFolder(os.path.join(data_dir, "test"),
-                                transform=get_transforms(False))
-
-    return (
-        DataLoader(train, batch_size=batch_size, shuffle=True),
-        DataLoader(val, batch_size=batch_size),
-        DataLoader(test, batch_size=batch_size),
-        train.classes
+    train_dataset = datasets.ImageFolder(
+        train_path,
+        transform=train_transform
     )
+
+    val_dataset = datasets.ImageFolder(
+        val_path,
+        transform=val_transform
+    )
+
+    test_dataset = datasets.ImageFolder(
+        test_path,
+        transform=val_transform
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+    return train_loader, val_loader, test_loader, train_dataset.classes
